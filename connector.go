02b10f66a41c230bb72b2b9119c0bdd42125912f
Yahya <webyahyaver@gmail.com>
2025-11-18 15:23:45 +0400

    final stack

---
+++ connector.go
@@ -0,0 +1,39 @@
+package main
+
+
+import(
+	"net"
+	"log"
+	"time"
+	"sync"
+)
+
+
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10000; i++  {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			log.Print("New Connection Starting")
+			// Inside the goroutine:
+			proxyConn, err := net.Dial("tcp", ":8080")
+			if err != nil {
+  		  log.Printf("Connect error: %v", err)
+    		return // <-- MUST RETURN
+			}
+			defer proxyConn.Close()
+
+		// Create a buffer with some data
+		data := []byte("Hello Gorgon Payload")
+		proxyConn.Write(data)
+
+		// Read the response (echo)
+		readBuf := make([]byte, 1024)
+		proxyConn.Read(readBuf)
+
+		time.Sleep(10 * time.Second)		}()
+	}
+	wg.Wait()
+}
--- proxy.go
+++ proxy.go
@@ -6,6 +6,13 @@ import (
 	"net"
 	"sync"
 	"time"
+	"os"
+	"os/signal"
+	"syscall"
+	"net/http"
+	_ "net/http/pprof"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // --- Configuration ---
@@ -13,6 +20,7 @@ const IDLE_TIMEOUT = 10 * time.Minute
 const DIAL_TIMEOUT = 2 * time.Second
 const HEALTH_CHECK_INTERVAL = 10 * time.Second
 const HEALTH_CHECK_TIMEOUT = 1 * time.Second
+
 
 // --- Globals ---
 
@@ -24,19 +32,83 @@ var bufferPool = sync.Pool{
 	},
 }
 
+type proxyMetrics struct {
+	current_active_connections  prometheus.Gauge
+	total_connections_handled prometheus.Counter
+	total_connection_failures *prometheus.CounterVec
+	connection_duration_seconds prometheus.Histogram
+}
+
+func NewMetrics(reg prometheus.Registerer) *proxyMetrics {
+	metrics := &proxyMetrics{
+	current_active_connections: prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "current_active_connections",
+		Help: "Current Active Connections",
+	}),
+	total_connections_handled: prometheus.NewCounter(
+			prometheus.CounterOpts{
+				Name: "total_connections_handled",
+				Help: "Total Number of Connections Handled",
+			},
+		),
+	total_connection_failures: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "total_connection_failures",
+				Help: "Total Number of Connections Failures.",
+			},
+			[]string{"reason"},
+		),
+	connection_duration_seconds: prometheus.NewHistogram(
+        	prometheus.HistogramOpts{
+				Name: "connection_duration_seconds",
+				Help: "Duration of connections in seconds.",
+				Buckets: prometheus.LinearBuckets(0.1, 0.1, 20),
+			},
+    ),
+}
+	reg.MustRegister(metrics.current_active_connections)
+	reg.MustRegister(metrics.total_connections_handled)
+	reg.MustRegister(metrics.total_connection_failures)
+	reg.MustRegister(metrics.connection_duration_seconds)
+	return metrics
+}
+
+
+
 type IServerProvider interface {
 	Next() string
 }
 
 // --- Main Function ---
 func main() {
-	upstreams := []string{
-		"google.com:80",
-		"httpbin.org:80",
-		"bad-host-does-not-exist:80",
-	}
 
+	reg := prometheus.NewRegistry()
+
+	metrics := NewMetrics(reg)
+
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
+	go func() {
+		log.Println("Metrics server listening on :8081")
+		if err := http.ListenAndServe(":8081", nil); err != nil {
+			log.Fatalf("Metrics server error: %v", err)
+		}
+	}()
+
+	go func() {
+	log.Println("Starting pprof server on :6060")
+
+	log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	upstreams := []string{
+		":7777",
+		":7778",
+	}
+	
 	var serverProvider = newRRServerProvider(upstreams)
+
+	var wg sync.WaitGroup
+
 
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -44,20 +116,34 @@ func main() {
 	}
 	defer listener.Close()
 
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+		sig := <- sigChan
+		log.Printf("Received signal: %v. Shutting down...", sig)
+
+		listener.Close()
+	}()
+
+
 	log.Printf("Gorgon TCP Proxy listening on :8080\n")
 
 	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
 			log.Printf("Failed to accept connection: %v", err)
-			continue
+			break
 		}
 
-		go handleConnection(clientConn, serverProvider)
+		wg.Add(1)
+
+		go handleConnection(clientConn, serverProvider, &wg, metrics)
 	}
 }
 
-func handleConnection(clientConn net.Conn, provider IServerProvider) {
+func handleConnection(clientConn net.Conn, provider IServerProvider, wg *sync.WaitGroup, metrics *proxyMetrics) {
+	defer wg.Done()
 	upstreamServer := provider.Next()
 	if upstreamServer == "" {
 		log.Println("No healthy upstream servers available.")
@@ -66,9 +152,22 @@ func handleConnection(clientConn net.Conn, provider IServerProvider) {
 	}
 
 	log.Printf("Handling connection from %s, proxying to %s\n", clientConn.RemoteAddr(), upstreamServer)
+	metrics.current_active_connections.Inc()
+	defer metrics.current_active_connections.Dec()
+
+	startTime := time.Now()
+	defer func() {
+        duration := time.Since(startTime)
+        // This is what calculates your p99!
+        metrics.connection_duration_seconds.Observe(duration.Seconds())
+    }()
+
+	metrics.total_connections_handled.Inc()
+
 
 	serverConn, err := net.DialTimeout("tcp", upstreamServer, DIAL_TIMEOUT)
 	if err != nil {
+		metrics.total_connection_failures.WithLabelValues("dial_error").Inc()
 		log.Printf("Failed to dial upstream '%s': %v", upstreamServer, err)
 		clientConn.Close()
 		return
